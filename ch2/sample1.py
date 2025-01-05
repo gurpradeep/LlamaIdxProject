0fda51e1663a37699b1097e96e5d42997c8d1c64
gurpradeep <gupta.pradeeep@gmail.com>
2025-01-05 15:36:21 +0530

    with latest llama version
    	modified:   ch2/sample1.py
    
    	modified:   ch2/sample1.py
    
    	modified:   .env

--- ch2/sample1.py
+++ ch2/sample1.py
@@ -1,8 +1,23 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import os
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import traceback
+from dotenv import load_dotenv
 
-documents = SimpleDirectoryReader('files').load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("summarize each document in a few sentences")
+load_dotenv()
 
-print(response)
+try:
+
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    files = os.path.join(current_directory, 'files')
+    documents = SimpleDirectoryReader(files).load_data()
+    hf_embedding = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    index = VectorStoreIndex.from_documents(documents,embed_model=hf_embedding, llm=None)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("summarize each document in a few sentences")
+
+    print(response)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    traceback.print_exc()
